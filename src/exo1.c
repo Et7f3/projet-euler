cb1a7dfaeca19d0e4ea24ada4c82b425c0f5e17c
Et7f3 <cadeaudeelie@gmail.com>
2018-03-29 11:39:40 +0000

    exo1 mis a jour

--- src/exo1.c
+++ src/exo1.c
@@ -5,7 +5,7 @@ int execute(int argc, char* argv[]);
 int main(int argc, char* argv[])
 {
 	if (argc <= 3)
-		return mode_d_emploi(3,
+		return mode_d_emploi(argv[0], 3,
 		"premier multiple, ex: 3",
 		"second multiple, ex: 5",
 		"maximum, ex: 1000");
@@ -15,15 +15,14 @@ int main(int argc, char* argv[])
 
 int execute(int argc, char* argv[])
 {
-	int max, m1, m2, i, ret;
+	int max, m1, m2, ret;
 	sscanf(argv[1], "%d", &m1);
 	sscanf(argv[2], "%d", &m2);
 	sscanf(argv[3], "%d", &max);
-	i = 0;
 	ret = 0;
-	while ( i++ < max - 1 )
-		if (i % m1 == 0 || i % m2 == 0)
-			ret += i;
+	while ( --max )
+		if (!(max % m1 && max % m2))
+			ret += max;
 	printf("ret: %d\n", ret);
 	return (EXIT_SUCCESS);
 }
